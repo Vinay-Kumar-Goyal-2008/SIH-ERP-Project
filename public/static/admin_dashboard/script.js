12607abac44ffdada1f4c4dec34d772086e25ce1
Vinay-Kumar-Goyal-2008 <vinaykumargoyal2008@gmail.com>
2025-09-08 22:56:14 +0530

    minor changes

--- public/static/admin_dashboard/script.js
+++ public/static/admin_dashboard/script.js
@@ -1,13 +1,13 @@
 async function alldata() {
-    document.querySelector('main div').style.display = 'none'
+    document.querySelector('.main0').style.display = 'none'
     document.querySelector('.loadingdata').style.display = 'block'
     let allstudentsnum = await fetch('/admin/totalstudents', {
         method: 'POST'
     })
+    allstudentsnum = await allstudentsnum.json()
     if (allstudentsnum.status = 200) {
-        document.querySelector('main>div').style.display = 'block'
+        document.querySelector('.main0').style.display = 'block'
         document.querySelector('.loadingdata').style.display = 'none'
-        allstudentsnum = await allstudentsnum.json()
         document.querySelector('.totalstudents .value').innerText = allstudentsnum.num
         document.querySelector('.totalfeescollected .value').innerText = allstudentsnum.amountpaid
         document.querySelector('.totalpendingfees .value').innerText = allstudentsnum.amountdue
